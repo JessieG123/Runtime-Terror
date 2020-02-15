246b273081d89dd6a4ba47e75ee4917a13fa6252
jessieguo <jessieguo863@gmail.com>
2020-02-15 13:33:21 -0700

    More changes

--- Character.java
+++ Character.java
@@ -1,5 +1,14 @@
 public class Character{
 
+    private double xCoordinates;
+    private double yCoordinates;
+    private boolean isInAir;
+
+    public Character(){
+        this.xCoordinates = x;
+        this.yCoordinates = y;
+        this.isInAir = air
+    }
     public double getX(){
         return xCoordinates;
     }
