7b2cd6ca8e13ad36152a54c179c0a78019a79c4f
jessieguo <jessieguo863@gmail.com>
2020-02-15 14:08:01 -0700

    handlers added

--- Player.java
+++ Player.java
@@ -1,7 +1,19 @@
 public class Player{
 
     //player class logic. need to be able to move around 
-    //check life 
+    //check life
+
+    private int lifeRemaining;
     
+    public Player(int lifeRemaining){
+        this.lifeRemaning = lifeRemaining;
+    }
+
+    public int getLifeRemaining(){
+        return lifeRemaining;
+    }
+
+    
+
 
 }
